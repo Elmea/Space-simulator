0f5e3e8ac2ddd3fda1a340f533b5302405acf154
Elmea <ninomass@hotmail.fr>
2023-02-20 16:02:20 +0100

    Remerge

--- GalaxySimulator/Assets/Scripts/Mouvement.cs
+++ GalaxySimulator/Assets/Scripts/Mouvement.cs
@@ -22,7 +22,10 @@ public class Mouvement : MonoBehaviour
     {
         VectorField field = other.GetComponent<VectorField>();
         if (field)
+        {
             rbody.AddForce(field.GetVectorFromPos(transform.position) / ForceDevider, ForceMode.Acceleration);
+            Debug.Log(rbody.velocity);
+        }
     }
     
     public void SetParameter(Vector3 parInitialSpeed)
