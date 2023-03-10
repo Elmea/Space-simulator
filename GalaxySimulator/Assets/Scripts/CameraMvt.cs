8054eb09ad21b4ba3fc4461d1b8b2d696e276f6c
t.samson <t.samson@student.isartdigital.com>
2023-03-10 16:00:46 +0100

    start Button show vector field

--- GalaxySimulator/Assets/Scripts/CameraMvt.cs
+++ GalaxySimulator/Assets/Scripts/CameraMvt.cs
@@ -64,7 +64,7 @@ public class CameraMvt : MonoBehaviour
     {
         transform.LookAt(target.transform);
 
-        Debug.Log(transform.forward);
+        //Debug.Log(transform.forward);
 
         posFromTarget = posXFromTarget + posYFromTarget +posZFromTarget;
 
