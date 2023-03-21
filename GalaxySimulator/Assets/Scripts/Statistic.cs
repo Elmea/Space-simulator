a3244e4202132782da42183bb47cbb36c9f98982
t.samson <t.samson@student.isartdigital.com>
2023-03-21 16:46:25 +0100

    begin show stat

---
+++ GalaxySimulator/Assets/Scripts/Statistic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Statistic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI name;
+    [SerializeField] TextMeshProUGUI speed;
+    [SerializeField] TextMeshProUGUI mass;
+    [SerializeField] TextMeshProUGUI acceleration;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
